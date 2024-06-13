4fcfd0978562e9795cf5fff8fd9dc48daccc5d11
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2024-06-13 12:27:44 +0700

    menampilkan menu Perform inorder traversal

--- BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree.cpp
@@ -74,6 +74,7 @@ int main() {
     while (true) {
         cout << "\nMenu" << endl;
         cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
 
 
 
